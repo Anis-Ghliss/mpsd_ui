c2fd4474ec6dfdb43af8dd05a221e5b239ffb9b1
Anis-Ghliss <ghlissanis@gmail.com>
2020-10-19 21:26:09 +0100

    'final'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import Routes from "./Routes";
+import "./App.css";
+import { AuthContext } from "./lib/authContext";
+import AddModal from "./components/AddPost";
 
-function App() {
+const App = () => {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [token, setToken] = useState("");
+
+  const [show, setShow] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    userHasAuthenticated(false);
+  };
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <AddModal show={show} handleClose={handleClose} setShow={setShow} />
+      <div className="App container">
+        <Navbar className="bg-light justify-content-between">
+          <Nav>
+            <Navbar.Brand>
+              <Link to="/">Posts</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Nav>
+          <Nav>
+            {isAuthenticated || localStorage.getItem("token") ? (
+              <>
+                <Nav.Item>
+                  <Nav.Link>Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link onClick={handleShow}>New Post</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                </Nav.Item>
+              </>
+            ) : (
+              <>
+                {" "}
+                <Nav.Item>
+                  <Nav.Link href="/signup">Sign Up</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="/login">Login</Nav.Link>
+                </Nav.Item>
+              </>
+            )}
+          </Nav>
+        </Navbar>
+        <AuthContext.Provider
+          value={{
+            isAuthenticated,
+            userHasAuthenticated,
+            token,
+            setToken,
+            show,
+            setShow,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Routes />
+        </AuthContext.Provider>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
